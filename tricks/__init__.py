f5a6ef907012e96f3994976472127e7e7f3ab2da
gbourdin <admin@gbourdin.com>
2014-03-05 22:52:59 -0300

    Refactored the __init__ file from the tricks module to import all the files that should be exported from it

--- tricks/__init__.py
+++ tricks/__init__.py
@@ -1,1 +1,10 @@
 # coding=utf-8
+
+from amiable import amiable_trick
+from c4tv import c4tv_trick
+from ftp import ftp_trick
+from lol import lol_trick
+from psychd import psychd_trick
+
+__all__ = ["amiable_trick", "c4tv_trick", "ftp_trick",
+           "lol_trick", "psychd_trick"]
